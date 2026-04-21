ce9f1590f8ee7b237b8e70fd41ad30e8a23b98a7
Karina Alves <karinaalves@MacBookPro.local>
2026-04-21 11:17:39 -0300

    primeiro commit

---
+++ ambiente.js
@@ -0,0 +1,95 @@
+let lista = document.getElementById('lista')
+
+let valores = [];
+
+function adicionar(){
+
+let numero = Number(document.getElementById('numero').value);
+
+
+let encontrado = false;
+
+
+if(numero >= 1 && numero <= 100){
+
+    for(let i = 0; i < valores.length; i++){
+    
+    if(valores[i]===numero){
+    encontrado= true;
+    alert('Valor já foi adicionado!')
+    return
+    }
+    
+}
+
+valores.push(numero)
+
+let item = document.createElement('option')
+    item.text = `Valor ${numero} adicionado`
+    lista.appendChild(item)
+
+}else if (numero === 0){
+alert('Valor inválido, digite um número!')
+
+}else if(numero > 100 || numero < 0){
+alert('Digite um número entre 1 e 100!')
+}
+
+
+}
+
+
+function finalizar(){
+
+let res = document.getElementById('res')
+
+            
+            
+
+    if(valores.length === 0){
+       alert('Adicione números à lista!') 
+       return
+        
+   }
+
+   let soma = 0
+   let maior = valores[0]
+   let menor = valores[0]
+
+   for(let i = 0; i < valores.length; i++){
+
+            soma = soma + valores[i]
+
+            if(valores[i] > maior){
+                maior = valores[i] }
+            
+            if(valores[i] < menor){
+               menor = valores[i] }
+   }
+
+
+
+    let media = soma / valores.length
+
+    res.innerHTML = ''
+    res.innerHTML+= `A soma total dos valores é ${soma} <br>`
+    res.innerHTML+= `O maior valor da lista é ${maior} <br>`
+    res.innerHTML+= `O menor valor da lista é ${menor} <br>`
+    res.innerHTML+= `A média dos valores é ${media} <br>`
+
+}
+
+    
+    //Calcular a média
+
+    //valores = valores.length[0]
+
+    //media =  valores + valores + valores /3   (i + i + i / 3) //Calculo estático
+
+   //media = valores ++ / valores.length[0]
+
+   //resultado =  media
+
+
+
+
